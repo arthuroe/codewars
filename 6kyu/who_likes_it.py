eb9fcf417fc6e6a582c666adecba788c253403fc
arthur <you@example.com>
2017-07-31 14:51:31 +0300

    who_likes_it

---
+++ 6kyu/who_likes_it.py
@@ -0,0 +1,92 @@
+'''
+You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
+
+Implement a function likes :: [String] -> String, which must take in input array, containing the names of people who like an item. It must return the display text as shown in the examples:
+
+likes [] // must be "no one likes this"
+likes ["Peter"] // must be "Peter likes this"
+likes ["Jacob", "Alex"] // must be "Jacob and Alex like this"
+likes ["Max", "John", "Mark"] // must be "Max, John and Mark like this"
+likes ["Alex", "Jacob", "Mark", "Max"] // must be "Alex, Jacob and 2 others like this"
+'''
+
+def likes(names):
+    if names == []:
+        return 'no one likes this'
+    elif len(names) == 1:
+        return names[0]+' likes this'
+    elif len(names) > 1 and len(names) < 4:
+        return ', '.join(names[name] for name in range(len(names)-1)) + ' and '+ names[-1] + ' like this'
+    else:
+        return ', '.join(names[name] for name in range(2)) +' and '+ str((len(names) - 2))+' others like this'
+
+
+'''---other solutions---'''
+def likes(names):
+    n = len(names)
+    return {
+        0: 'no one likes this',
+        1: '{} likes this', 
+        2: '{} and {} like this', 
+        3: '{}, {} and {} like this', 
+        4: '{}, {} and {others} others like this'
+    }[min(4, n)].format(*names[:3], others=n-2)
+
+
+def likes(names):
+    formats = {
+            0: "no one likes this",
+            1: "{} likes this",
+            2: "{} and {} like this",
+            3: "{}, {} and {} like this",
+            4: "{}, {} and {others} others like this"
+        }
+    n = len(names)
+    return formats[min(n,4)].format(*names, others=n-2)
+        
+
+def likes(names):
+    # make a dictionary d of all the possible answers. Keys are the respective number
+    # of people who liked it.
+    
+    # {} indicate placeholders. They do not need any numbers but are simply replaced/formatted
+    # in the order the arguments in names are given to format
+    # {others} can be replaced by its name; below the argument "others = length - 2"
+    # is passed to str.format()
+    d = {
+        0: "no one likes this",
+        1: "{} likes this",
+        2: "{} and {} like this",
+        3: "{}, {} and {} like this",
+        4: "{}, {} and {others} others like this"
+        }
+    length = len(names)
+    # d[min(4, length)] insures that the appropriate string is called from the dictionary
+    # and subsequently returned. Min is necessary as len(names) may be > 4
+    
+    # The * in *names ensures that the list names is blown up and that format is called
+    # as if each item in names was passed to format individually, i. e.
+    # format(names[0], names[1], .... , names[n], others = length - 2
+    return d[min(4, length)].format(*names, others = length - 2)
+
+
+
+'''---test cases---'''
+Test.describe('Basic tests')
+Test.assert_equals(likes([]), 'no one likes this')
+Test.assert_equals(likes(['Peter']), 'Peter likes this')
+Test.assert_equals(likes(['Jacob', 'Alex']), 'Jacob and Alex like this')
+Test.assert_equals(likes(['Max', 'John', 'Mark']), 'Max, John and Mark like this')
+Test.assert_equals(likes(['Alex', 'Jacob', 'Mark', 'Max']), 'Alex, Jacob and 2 others like this')
+
+Test.describe("Random tests")
+from random import randint, shuffle
+sol=lambda n: 'no one likes this' if len(n)==0 else n[0]+' likes this' if len(n)==1 else n[0]+' and '+n[1]+' like this' if len(n)==2 else n[0]+', '+n[1]+' and '+n[2]+' like this' if len(n)==3 else n[0]+', '+n[1]+' and '+str(len(n)-2)+' others like this'
+base=["Sylia Stingray", "Priscilla S. Asagiri", "Linna Yamazaki", "Nene Romanova", "Nigel", "Macky Stingray", "Largo", "Brian J. Mason", "Sylvie", "Anri", "Leon McNichol", "Daley Wong", "Galatea", "Quincy Rosenkreutz"]
+
+for _ in xrange(40):
+    shuffle(base)
+    names=base[:randint(0,len(base)-1)]
+    Test.it("Testig for %s" %(", ".join(names) if len(names)>0 else "none"))
+    Test.assert_equals(likes(names),sol(names),"It should work for random inputs too")
+
